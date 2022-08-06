4ad24abd2ca478a26d26d462e13510e2720c8b91
allpha23 <allpha23@gmail.com>
2022-08-06 02:08:21 -0300

    feat: add error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
 import carRouter from './routes/cars';
+import errorHandler from './middlewares/error';
 
 const app = express();
 app.use(express.json());
 app.use(carRouter);
+app.use(errorHandler);
 
 export default app;
