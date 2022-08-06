9411dfae8a203b51fa934c49f4fcd29949231bb4
allpha23 <allpha23@gmail.com>
2022-08-06 03:06:58 -0300

    feat(test): add car model test

---
+++ src/tests/unit/models/carModel.test.ts
@@ -0,0 +1,24 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+const { expect } = chai;
+import CarModel from '../../../models/Car';
+import { Model } from 'mongoose';
+import { carMock, carMockWithId } from '../../mocks/carMock';
+
+describe('Car Model', () => {
+  const carModel = new CarModel();
+
+  before(async () => {
+    sinon.stub(Model, 'create').resolves(carMockWithId);
+  });
+
+  after(()=>{
+    sinon.restore();
+  })
+  describe('Create a car', () => {
+    it('Succesfully created', async () => {
+      const newCar = await carModel.create(carMock);
+      expect(newCar).to.be.deep.equal(carMockWithId);
+    });
+  })
+});
