1ddcdbc2c2a403310f38df0f8b3320b27e60d197
allpha23 <allpha23@gmail.com>
2022-08-06 11:44:15 -0300

    feat(test): add read and readOne service test

--- src/tests/unit/services/carService.test.ts
+++ src/tests/unit/services/carService.test.ts
@@ -2,10 +2,10 @@ import * as sinon from 'sinon';
 import chai from 'chai';
 const { expect } = chai;
 import { ZodError } from 'zod';
-// import { ErrorTypes } from '../../../errors/catalog';
 import CarModel from '../../../models/Car';
 import CarService from '../../../services/Car';
-import { carMock, carMockWithId } from '../../mocks/carMock';
+import { carMock, carMockWithId, allCarMock } from '../../mocks/carMock';
+import { ErrorTypes } from '../../../errors/catalog';
 
 describe('Car Service', () => {
   const carModel = new CarModel();
@@ -13,12 +13,16 @@ describe('Car Service', () => {
 
   before(async () => {
     sinon.stub(carModel, 'create').resolves(carMockWithId);
+    sinon.stub(carModel, 'read').resolves(allCarMock);
+    sinon.stub(carModel, 'readOne')
+      .onCall(0).resolves(carMockWithId)
+      .onCall(1).resolves(null);
   });
 
   after(()=>{
     sinon.restore();
   })
-  describe('Create a car', () => {
+  describe('Create Car', () => {
     it('Succes', async () => {
       const newCar = await carService.create(carMock);
       expect(newCar).to.be.deep.equal(carMockWithId);
@@ -29,6 +33,28 @@ describe('Car Service', () => {
         await carService.create({} as any);
       } catch (error) {
         expect(error).to.be.instanceOf(ZodError);
+      }
+    });
+  })
+
+  describe('Read Car', () => {
+    it('Succes', async () => {
+      const cars = await carService.read();
+      expect(cars).to.be.deep.equal(allCarMock);
+    });
+  })
+
+  describe('ReadOne Car', () => {
+    it('Succes', async () => {
+      const car = await carService.readOne(carMockWithId._id);
+      expect(car).to.be.deep.equal(carMockWithId);
+    });
+
+    it('Failure', async () => {
+      try {
+        await carService.readOne(carMockWithId._id);
+      } catch (error: any) {
+        expect(error.message).to.be.deep.equal(ErrorTypes.EntityNotFound);
       }
     });
   })
