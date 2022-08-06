5332825100e5547d62666d679c09633f96e90b64
allpha23 <allpha23@gmail.com>
2022-08-06 11:43:37 -0300

    feat(test): add all cars mock

--- src/tests/mocks/carMock.ts
+++ src/tests/mocks/carMock.ts
@@ -19,7 +19,29 @@ const carMockWithId: ICar & { _id: string } = {
   doorsQty: 2
 }
 
+const allCarMock: ICar[] & { _id: string }[] = [
+  {
+    _id: "4edd40c86762e0fb12000003",
+    model: "Ferrari Maranello",
+    year: 1963,
+    color: "red",
+    buyValue: 3500000,
+    seatsQty: 2,
+    doorsQty: 2
+  },
+  {
+    _id: "62ee09fe5ad84407c9741c7a",
+    model: "Lamborghini",
+    year: 1963,
+    color: "black",
+    buyValue: 3500000,
+    seatsQty: 2,
+    doorsQty: 2
+  }
+];
+
 export {
   carMock,
-  carMockWithId
+  carMockWithId,
+  allCarMock
 };
