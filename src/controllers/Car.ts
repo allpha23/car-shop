fe6d3938f0258efa5d2f1b7b687eb290feccf52a
allpha23 <allpha23@gmail.com>
2022-08-06 03:47:40 -0300

    feat(controller): add read and readOne

--- src/controllers/Car.ts
+++ src/controllers/Car.ts
@@ -6,7 +6,17 @@ export default class CarController {
   constructor(private _service: IService<ICar>) {}
 
   public async create(req: Request, res: Response) {
-    const results = await this._service.create(req.body);
-    return res.status(201).json(results);
+    const result = await this._service.create(req.body);
+    return res.status(201).json(result);
+  }
+
+  public async read(req: Request, res: Response) {
+    const results = await this._service.read();
+    return res.status(200).json(results);
+  }
+
+  public async readOne(req: Request, res: Response) {
+    const result = await this._service.readOne(req.params.id);
+    return res.status(200).json(result);
   }
 }
