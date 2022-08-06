0e565f3202801631c0feb29de70d68cc4f25e14d
allpha23 <allpha23@gmail.com>
2022-08-06 11:42:52 -0300

    feat(test): add find and findOne model test

--- src/tests/unit/models/carModel.test.ts
+++ src/tests/unit/models/carModel.test.ts
@@ -3,22 +3,39 @@ import chai from 'chai';
 const { expect } = chai;
 import CarModel from '../../../models/Car';
 import { Model } from 'mongoose';
-import { carMock, carMockWithId } from '../../mocks/carMock';
+import { carMock, carMockWithId, allCarMock } from '../../mocks/carMock';
 
 describe('Car Model', () => {
   const carModel = new CarModel();
 
   before(async () => {
     sinon.stub(Model, 'create').resolves(carMockWithId);
+    sinon.stub(Model, 'find').resolves(allCarMock);
+    sinon.stub(Model, 'findOne').resolves(carMockWithId);
   });
 
   after(()=>{
     sinon.restore();
   })
+
   describe('Create a car', () => {
     it('Succesfully created', async () => {
       const newCar = await carModel.create(carMock);
       expect(newCar).to.be.deep.equal(carMockWithId);
+    });
+  })
+
+  describe('Searching all cars', () => {
+    it('Succesfully found all cars', async () => {
+      const cars = await carModel.read();
+      expect(cars).to.be.deep.equal(allCarMock);
+    });
+  })
+
+  describe('Searching a car', () => {
+    it('Succesfully found', async () => {
+      const cars = await carModel.readOne(carMockWithId._id);
+      expect(cars).to.be.deep.equal(carMockWithId);
     });
   })
 });
